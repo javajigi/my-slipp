e2f1a675394fb048473e022e85a5c3d045a895d6
javajigi <javajigi@gmail.com>
2013-03-25 22:07:19 +0900

    2자리 숫자에 대한 combinations을 구할 수 있는 함수 추가함.

--- src/datastructure/Combinations.c
+++ src/datastructure/Combinations.c
@@ -7,7 +7,7 @@ void swap(char *i, char *j) {
 }
 
 void print_combinations(char *values, int start, int end) {
-	for(int i=start; i <= end; i++) {
+	for (int i = start; i <= end; i++) {
 		printf(" %c", values[i]);
 		if (i == end) {
 			printf(" \n");
@@ -18,15 +18,15 @@ void print_combinations(char *values, int start, int end) {
 }
 
 void combinations_two(char *values, int n, int r) {
-	for(int i=0; i<=r;i++) {
-		swap(values, values + i);
-		print_combinations(values, 1, r);
-		swap(values + i, values);
+	for (int start = 0; start <= n - r + 1; start++) {
+		for (int i = start; i <= n - r; i++) {
+			printf(" %c, %c\n", values[start], values[i + 1]);
+		}
 	}
 }
 
 void combinations_three(char *values, int n, int r) {
-	for(int i=0; i<=r;i++) {
+	for (int i = 0; i < n; i++) {
 		swap(values, values + i);
 		print_combinations(values, 1, r);
 		swap(values + i, values);
@@ -34,12 +34,9 @@ void combinations_three(char *values, int n, int r) {
 }
 
 void combinations_four(char *values, int n, int r) {
-	for(int i=0; i<=r;i++) {
+	for (int i = 0; i <= r; i++) {
 		swap(values, values + i);
 		print_combinations(values, 1, r);
 		swap(values + i, values);
 	}
-}
-
-void combinations(char *values, int start, int end) {
 }
--- test/datastructure/TestCombinations.c
+++ test/datastructure/TestCombinations.c
@@ -10,16 +10,16 @@ void tearDown(void) {
 }
 
 void test_combinations_two_length(void) {
-	char values[] = { 'a', 'b', 'c' };
-	combinations_two(values, 3, 2);
+	char values[] = { 'a', 'b', 'c', 'd', 'e' };
+	combinations_two(values, 5, 2);
 }
 
-void test_combination_three_length(void) {
-	char values[] = { 'a', 'b', 'c', 'd', 'e' };
-	combinations_three(values, 5, 3);
-}
-
-void test_combination_four_length(void) {
-	char values[] = { 'a', 'b', 'c', 'd', 'e' };
-	combinations_four(values, 5, 4);
-}
+//void test_combination_three_length(void) {
+//	char values[] = { 'a', 'b', 'c', 'd', 'e' };
+//	combinations_three(values, 5, 3);
+//}
+//
+//void test_combination_four_length(void) {
+//	char values[] = { 'a', 'b', 'c', 'd', 'e' };
+//	combinations_four(values, 5, 4);
+//}
